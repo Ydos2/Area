d9d1dc20c657e495382ab7f58de559ba56450733
Mathias Ressort <mathias.ressort@epitech.eu>
2022-02-05 18:42:28 +0100

    Add template pages

---
+++ client/lib/models/Recommendation.dart
@@ -0,0 +1,32 @@
+class Recommendation {
+  final String? name, source, text;
+
+  Recommendation({this.name, this.source, this.text});
+}
+
+final List<Recommendation> demo_recommendations = [
+  Recommendation(
+    name: "Mettre des trucs",
+    source: "Lorem Ipsum",
+    text:
+        "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.",
+  ),
+  Recommendation(
+    name: "Mettre des trucs",
+    source: "Lorem Ipsum",
+    text:
+        "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.",
+  ),
+  Recommendation(
+    name: "Mettre des trucs",
+    source: "Lorem Ipsum",
+    text:
+        "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.",
+  ),
+  Recommendation(
+    name: "Mettre des trucs",
+    source: "Lorem Ipsum",
+    text:
+        "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.",
+  ),
+];
---
+++ client/lib/screens/main/components/side_menu.dart
@@ -0,0 +1,89 @@
+import 'package:flutter/material.dart';
+import 'package:area/constants.dart';
+import 'package:flutter_svg/svg.dart';
+
+import 'area_info_text.dart';
+import 'coding.dart';
+import 'knowledges.dart';
+import 'my_info.dart';
+import 'skills.dart';
+
+class SideMenu extends StatelessWidget {
+  const SideMenu({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Drawer(
+      child: SafeArea(
+        child: Column(
+          children: [
+            const MyInfo(),
+            Expanded(
+              child: SingleChildScrollView(
+                padding: const EdgeInsets.all(defaultPadding),
+                child: Column(
+                  children: [
+                    const AreaInfoText(
+                      title: "Truc",
+                      text: "Autre truc",
+                    ),
+                    const Skills(),
+                    const SizedBox(height: defaultPadding),
+                    const Coding(),
+                    const Knowledges(),
+                    const Divider(),
+                    const SizedBox(height: defaultPadding / 2),
+                    TextButton(
+                      onPressed: () {},
+                      child: FittedBox(
+                        child: Row(
+                          children: [
+                            Text(
+                              "LOG OUT",
+                              style: TextStyle(
+                                color: Theme.of(context)
+                                    .textTheme
+                                    .bodyText1!
+                                    .color,
+                              ),
+                            ),
+                            const SizedBox(width: defaultPadding / 2),
+                            SvgPicture.asset("assets/icons/download.svg")
+                          ],
+                        ),
+                      ),
+                    ),
+                    Container(
+                      margin: const EdgeInsets.only(top: defaultPadding),
+                      color: const Color(0xFF24242E),
+                      child: Row(
+                        children: [
+                          const Spacer(),
+                          IconButton(
+                            onPressed: () {},
+                            icon: SvgPicture.asset("assets/icons/linkedin.svg"),
+                          ),
+                          IconButton(
+                            onPressed: () {},
+                            icon: SvgPicture.asset("assets/icons/github.svg"),
+                          ),
+                          IconButton(
+                            onPressed: () {},
+                            icon: SvgPicture.asset("assets/icons/twitter.svg"),
+                          ),
+                          const Spacer(),
+                        ],
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
