0b2da7606c5482b2d439d8a4e3e9300346b19ba4
Mathias Combalbert <mathias.combalbert@epitech.eu>
2022-03-01 15:22:13 +0100

    ADD: fully fonctionnal UI oflogin form

--- client/lib/screens/Login/home_view.dart
+++ client/lib/screens/Login/home_view.dart
@@ -2,14 +2,16 @@ import 'package:flutter/material.dart';
 import 'package:area/screens/Login/home_desktop.dart';
 import 'package:area/screens/Login/home_mobile.dart';
 import 'package:responsive_builder/responsive_builder.dart';
+import 'package:area/screens/Login/mobile_view/ui.dart';
+
 
 class HomeView extends StatelessWidget {
   static const String route = '/login';
   @override
   Widget build(BuildContext context) {
     return ScreenTypeLayout(
-      mobile: HomeMobile(),
-      desktop: HomeDesktop(),
+      mobile: const mobile_view(),
+      desktop: const mobile_view(),
     );
   }
 }
